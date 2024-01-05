897bb63c8851544df8e96a038d6092ae429687b4
Joey Gibson <joey@joeygibson.com>
2024-01-04 21:02:19 -0500

    more regex attempts

--- regular-expressions.cob
+++ regular-expressions.cob
@@ -1,71 +1,53 @@
         IDENTIFICATION DIVISION.
-        PROGRAM-ID. Regular-Expression.
-        author. Joey Gibson.
-        DATE-WRITTEN. 2023-12-29.
-
-        ENVIRONMENT DIVISION.
-        configuration section.
-      *  repository.
-      *      function all intrinsic regular-expression.
+        PROGRAM-ID. RegexTest.
 
         DATA DIVISION.
-        
-        working-storage section.
-        01  WS-string-1 PIC X(20) VALUE "8eight1".
-        01  WS-pattern pic x(57) value 
+        WORKING-STORAGE SECTION.
+        01 WS-PATTERN PIC X(100) VALUE 
             "(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))".
-        01  WS-return-code pic s9(4) comp-5.
-        01  WS-regex pic s9(4) comp-5.
-        01  WS-error-code pic s9(4) comp-5.
-        01  WS-error-offset pic s9(4) comp-5.
-        01  WS-matches.
-            05  WS-matches-1 occurs 10 times.
-              10  WS-matches-1-start pic s9(4) comp-5.
-              10  WS-matches-1-end pic s9(4) comp-5.
+        01 WS-STRING PIC X(100) VALUE "8eight1".
+        01 WS-REGEX USAGE POINTER.
+        01 WS-ERROR-CODE PIC S9(4) COMP-5.
+        01 WS-ERROR-OFFSET PIC S9(4) COMP-5.
+        01 WS-OVECTOR OCCURS 30 TIMES PIC S9(4) COMP-5.
+        01 WS-RESULT PIC S9(4) COMP-5.
+        01 WS-I PIC S9(4) COMP-5.
 
         PROCEDURE DIVISION.
-            call "pcre_compile" using 
-                WS-pattern
-                LENGTH OF WS-pattern
+            CALL "pcre_compile" USING 
+                WS-PATTERN
+                LENGTH OF WS-PATTERN
                 0
                 ADDRESS OF WS-ERROR-CODE
                 ADDRESS OF WS-ERROR-OFFSET
                 0
-              returning WS-REGEX.
-       
-            if WS-regex = LOW-VALUES
-                display "Error compiling regex"
-                display "Error code: " WS-ERROR-CODE
-                display "Error offset: " WS-ERROR-OFFSET
-                stop run
-            end-if.
+            RETURNING WS-REGEX.
 
+            IF WS-REGEX = NULL
+                DISPLAY "Error compiling regex"
+                DISPLAY "Error code: " WS-ERROR-CODE
+            ELSE
+                CALL "pcre_exec" USING 
+                    WS-REGEX
+                    NULL
+                    WS-STRING
+                    LENGTH OF WS-STRING
+                    0
+                    0
+                    ADDRESS OF WS-OVECTOR(1)
+                    LENGTH OF WS-OVECTOR
+                RETURNING WS-RESULT.
 
-      *     call "pcre_exec" using 
-      *       WS-regex
-      *       0
-      *       WS-string-1
-      *       length of WS-string-1
-      *       0
-      *       0
-      *       WS-matches
-      *       0
-      *       returning WS-return-code
-      *     end-call
-      *     if WS-return-code = 0
-      *         display "No match found"
-      *     else
-      *         display "Match found"
-      *         display "Number of matches: " WS-RETURN-CODE
-      *         display 'matches: ' WS-MATCHES
-      *
-      *         display "Group 1: " WS-MATCHES-1(1)
-      *         display "Group 2: " WS-MATCHES-1(2)
-      *         display "Group 3: " WS-MATCHES-1(3)
-      *         call "Group" using WS-string-1 1
-      *           giving extracted-string
-      *
-      *         display "Group 1: " extracted-string
-      *     end-if.
-
-        
+                IF WS-RESULT >= 0
+                    DISPLAY "Match found"
+                    PERFORM VARYING WS-I FROM 1 BY 1 
+                      UNTIL WS-I > WS-RESULT
+                        DISPLAY "Capture group " WS-I ": "
+                        DISPLAY WS-STRING(WS-OVECTOR(WS-I * 2 - 1) + 
+                          1:WS-OVECTOR(WS-I * 2))
+                    END-PERFORM
+                ELSE
+                    DISPLAY "Match not found"
+                END-IF
+            
+            STOP RUN.
