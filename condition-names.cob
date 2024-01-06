2c6961aeccb85dd88ed64e3eb8dfa45230dd2026
Joey Gibson <joey@joeygibson.com>
2024-01-05 23:29:41 -0500

    more conditions

--- condition-names.cob
+++ condition-names.cob
@@ -11,6 +11,12 @@
                      "j" THRU "n", "p" THRU "t", "v" THRU "z".
               88 Digit value "0" THRU "9".
               88 ValidCharacter value "a" THRU "z", "0" THRU "9".
+       01  Grade PIC X.
+              88 Is-A value "A", "a".
+              88 Is-B value "B", "b".
+              88 Is-C value "C", "c".
+              88 Is-D value "D", "d".
+              88 Is-F value "F", "f".
        
        PROCEDURE DIVISION.
        Begin.
@@ -27,4 +33,39 @@
               END-EVALUATE
               ACCEPT CharIn
            END-PERFORM
+
+           MOVE 'C' to Grade.
+           
+           IF Grade = 'A' or 'B'
+              DISPLAY 'Great!'
+           ELSE IF Grade = 'C' or 'D'
+              DISPLAY 'OK'
+           ELSE
+              DISPLAY 'You failed.'
+           END-IF
+
+           DISPLAY '---'
+
+           IF Is-A
+              DISPLAY 'GreatX!'
+           ELSE IF Is-B
+              DISPLAY 'OK'
+           ELSE
+              DISPLAY 'You failed.'
+           END-IF
+
+           DISPLAY '---'
+
+           IF Is-C
+              DISPLAY "X entered"
+           END-IF
+
+           IF Is-D
+              DISPLAY "Y entered"
+           END-IF
+
+           IF Is-F
+              DISPLAY "Z entered"
+           END-IF
+
            STOP RUN.
