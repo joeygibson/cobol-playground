f35edd6b18f672eb08a24ac0b333484d27d90b99
Joey Gibson <joey@joeygibson.com>
2024-01-07 11:16:17 -0500

    shops, 2

--- shops.cob
+++ shops.cob
@@ -1,6 +1,5 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Shops.
-       AUTHOR. Joey Gibson.
       * Multiple record types in the same sequential file.
 
        ENVIRONMENT DIVISION.
@@ -17,6 +16,7 @@
         02 RecTypeCode PIC X.
           88 ShopHeader VALUE 'H'.
           88 ShopSale VALUE 'S'.
+          88 ShopFooter VALUE 'F'.
         02 ShopId PIC X(5).
         02 ShopLocation PIC X(30).
 
@@ -26,20 +26,32 @@
         02 QtySold PIC 9(3).
         02 ItemCost PIC 999V99.
 
+       01 ShopSalesCount.
+        02 TypeCode PIC X.
+        02 RecCount PIC 9(5).
+
        WORKING-STORAGE SECTION.
        01 PrnShopSalesTotal.
         02 FILLER PIC X(21) VALUE 'Total sales for shop '.
         02 PrnShopId PIC X(5).
         02 PrnShopTotal PIC $,$$$,$$9.99.
 
+       01 PrnErrorMessage.
+        02 FILLER             PIC X(15) VALUE "Error on Shop: ".
+        02 PrnErrorShopId     PIC X(5).
+        02 FILLER             PIC X(10) VALUE " RCount = ".
+        02 PrnRecCount        PIC 9(5).
+        02 FILLER             PIC X(10) VALUE " ACount = ".
+        02 PrnActualCount     PIC 9(5).
+
        01 ShopTotal PIC 9(5)V99.
+       01 ActualCount PIC 9(5).
 
        PROCEDURE DIVISION.
        ShopSalesSummary.
            OPEN INPUT ShopReceiptsFile
-           READ ShopReceiptsFile
-             AT END SET EndOfShopReceiptsFile TO TRUE
-           END-READ
+           
+           PERFORM GetHeaderRec
 
            PERFORM SummarizeCountrySales
              UNTIL EndOfShopReceiptsFile
@@ -56,14 +68,26 @@
            END-READ
 
            PERFORM SummarizeShopSales
-             UNTIL ShopHeader OR EndOfShopReceiptsFile
+             VARYING ActualCount FROM 0 BY 1 UNTIL ShopFooter
 
-           MOVE ShopTotal TO PrnShopTotal
+           IF RecCount = ActualCount
+             MOVE ShopTotal TO PrnShopTotal
+             DISPLAY PrnShopSalesTotal
+           ELSE
+             MOVE RecCount TO PrnRecCount 
+             MOVE ActualCount TO PrnActualCount
+             DISPLAY PrnErrorMessage
+           END-IF.
 
-           DISPLAY PrnShopSalesTotal.
+           PERFORM GetHeaderRec.
 
        SummarizeShopSales.
            COMPUTE ShopTotal = ShopTotal + (QtySold * ItemCost)
+           READ ShopReceiptsFile
+             AT END SET EndOfShopReceiptsFile TO TRUE
+           END-READ.
+
+       GetHeaderRec.
            READ ShopReceiptsFile
              AT END SET EndOfShopReceiptsFile TO TRUE
            END-READ.
